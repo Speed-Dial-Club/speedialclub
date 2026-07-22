878f5ab53f47ec63b3611ce7e5cb59254a405475
Speed Dial Club <speedialclub@gmail.com>
2026-07-22 19:45:51 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,15 +1,26 @@
 const CONFIG = {
 
-    API_URL: "https://script.google.com/macros/s/AKfycbzLSv1pGkyUyTVxvM_fuQ5LKYx53B8KFOF0H7nMElFsbAbcq8jsHcfLo7Ak1bGJjHGh8g/exec",
+    API: {
 
-    MAX_FILE_SIZE: 10 * 1024 * 1024,
+        URL: "https://script.google.com/macros/s/AKfycbzLSv1pGkyUyTVxvM_fuQ5LKYx53B8KFOF0H7nMElFsbAbcq8jsHcfLo7Ak1bGJjHGh8g/exec",
 
-    MAX_UPLOAD_MB: 10,
+        ACTIONS: {
+            GET_CAMPAIGNS: "campaigns",
+            SUBMIT_PAYOUT: "submitPayout"
+        }
 
-    ALLOWED_FILE_TYPES: [
-        "application/pdf"
-    ],
+    },
 
-    DUPLICATE_STATUS: "Pending"
+    UPLOAD: {
+
+        MAX_FILE_SIZE: 10 * 1024 * 1024,
+
+        MAX_UPLOAD_MB: 10,
+
+        ALLOWED_FILE_TYPES: [
+            "application/pdf"
+        ]
+
+    }
 
 };
