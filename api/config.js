9c5c6f5f7ac10227a6dd9f2c94240b21e418cd23
Speed Dial Club <speedialclub@gmail.com>
2026-07-22 17:25:02 +0530

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+
+    API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+
+    MAX_FILE_SIZE: 10 * 1024 * 1024,
+
+    MAX_UPLOAD_MB: 10,
+
+    ALLOWED_FILE_TYPES: [
+        "application/pdf"
+    ],
+
+    DUPLICATE_STATUS: "Pending"
+
+};
