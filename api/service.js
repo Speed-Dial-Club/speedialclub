697fc16ca627f764635924875f618dd8f89e2e3c
Speed Dial Club <speedialclub@gmail.com>
2026-07-23 15:04:43 +0530

    Update service.js

--- api/service.js
+++ api/service.js
@@ -35,10 +35,6 @@ async function apiPost(payload) {
 
         method: "POST",
 
-        headers: {
-            "Content-Type": "application/json"
-        },
-
         body: JSON.stringify(payload)
 
     });
